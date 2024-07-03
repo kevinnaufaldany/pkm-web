4af0f895b3f8888d268fa0d3f90d5fa05a531048
kevinnaufaldany <kevinbai2584@gmail.com>
2024-07-03 23:49:15 +0700

    read more + finishing

---
+++ product/product.js
@@ -0,0 +1,30 @@
+//Fungsi mengganti gambar saat di klik
+function img(anything) {
+  document.querySelector(".slide").src = anything;
+}
+
+function change(change) {
+  const line = document.querySelector(".home");
+  line.style.background = change;
+}
+
+//Fungsi perhitungan harga
+let valueCount = 1;
+let increment = document.querySelector(".increment");
+let decrement = document.querySelector(".decrement");
+let count = document.querySelector("#quantity");
+let totalcount = document.querySelector("#price");
+
+increment.addEventListener("click", () => {
+  let add = ++valueCount;
+  count.value = add;
+  totalcount.innerHTML = add * 100000;
+});
+
+decrement.addEventListener("click", () => {
+  if (valueCount >= 2) {
+    let sub = --valueCount;
+    count.value = sub;
+    totalcount.innerHTML = sub * 100000;
+  }
+});
