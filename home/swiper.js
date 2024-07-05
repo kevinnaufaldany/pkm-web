e0d1b45845d8e404eb1e2af2da6a6f8f5aa780b8
kevinnaufaldany <kevinbai2584@gmail.com>
2024-07-05 23:18:18 +0700

    read more perbaiki

--- home/swiper.js
+++ home/swiper.js
@@ -1,13 +1,8 @@
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    centeredSlides: true,
-    spaceBetween: 25,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
