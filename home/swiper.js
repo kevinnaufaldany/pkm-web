54088acd78076cdfedf93aed751b9c0b6019bd3d
kevinnaufaldany <kevinbai2584@gmail.com>
2024-07-19 20:28:46 +0700

    nav menu done

--- home/swiper.js
+++ home/swiper.js
@@ -1,12 +1,11 @@
 //SWIPER
 
-const swiperFavorites = new Swiper('.slide-content-swiper', {
+const swiperFavorites = new Swiper('#artikel .slide-content-swiper', {
   loop: true,
   grabCursor: true,
   slidesPerView: 3,
   slidesPerView: 'auto',
   centeredSlides: 'auto',
-  spaceBetween: 50,
   pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -18,5 +17,22 @@ let more = document.querySelectorAll(".more");
 for (let i = 0; i < more.length; i++) {
   more[i].addEventListener("click", function () {
     more[i].parentNode.classList.toggle("active");
+  });
+}
+
+//NAV MENU
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
   });
 }
